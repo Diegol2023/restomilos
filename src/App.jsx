5d7c0c7c4df479a2e91668e7e9f5f9be443e61ee
Diegol2023 <logicadigital@hotmail.com>
2024-12-05 13:02:41 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1 style={{ color: '#c09e06' }}> Milos Resto and Bar</h1>
+        <img src="/public/img/logo.png" alt="resto" width="80%" height="300px" />
+      </div>
+    </>
+  )
+}
+
+export default App
