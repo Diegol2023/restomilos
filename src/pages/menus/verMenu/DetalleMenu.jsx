f254e5d48f3500f8d372982fbc6f3914ecbd4dde
Diegol2023 <logicadigital@hotmail.com>
2024-12-26 13:03:36 -0300

    cuarto commit context

--- src/pages/menus/verMenu/DetalleMenu.jsx
+++ src/pages/menus/verMenu/DetalleMenu.jsx
@@ -1,24 +1,31 @@
 /* eslint-disable react/prop-types */
-
+import './DetalleMenu.css'
 import { useNavigate } from 'react-router-dom';
 const DetalleMenu = ({menu}) => {
     const navigate = useNavigate();
     const hanldeClick = () => {
-        navigate(`/menu/${menu.source.id}`);
+        navigate(`/`);
     };
     return (
-        <div className='contenedor' onClick={hanldeClick}>  
+        <>
+            <div className='contenedorDetalle' >  
             <img src={menu.urlToImage} alt="resto" width="30%" height="300px" className='imagen'/>     
-            <div className='datos'>
+                <div className='datos'>
                 <h2 className='titulo'>{menu.title}</h2>
-                <div className='subdatos'>
-                    <p className='contenido'>{menu.content}</p>
-                    <p className='precio'>Precio:  ${menu.precio}</p>
+                    <div className='subdatos'>
+                        <p className='contenido'>{menu.content}</p>
+                        <p className='precio'>Precio:  ${menu.precio}</p>
+                    </div>
                 </div>
+        
+        
             </div>
+            <div className='botones'>
+                <button className='boton' onClick={hanldeClick}>Volver</button> 
+            </div>
+        </>
+        
 
-            
-        </div>
         
     );          
 }   
